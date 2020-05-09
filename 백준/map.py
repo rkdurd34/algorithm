2498d51a2b7987fc89fcb7bc8778cd71e58ebc3d
rkdrud34 <rkdrud34@nate.com>
2020-05-09 13:34:09 +0900

    dasda

--- 백준/map.py
+++ 백준/map.py
@@ -4,10 +4,12 @@
 # num_input = int(input())
 # a=[]
 # for i in range(num_input):
-#     # a.append(list(map(split,input().split())))
+#     a.append(list(map(split,input().split())))
 #     a = input().split()
 #     print(a)
 li=[1,2,3]
 result = list(map(lambda i:i**2, li))
 
+list_1 = ['23','21', '13','12','11',]
+result = list(sorted(list_1, key=lambda x: (x[0], x[1])))
 print(result)
