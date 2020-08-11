51b3d6c4db0ccf1576872ffab6fdb9b6ee799aac
rkdrud34 <rkdrud34@nate.com>
2020-08-11 16:11:33 +0900

    평범한 배낭

---
+++ 백준/1932_정수삼각형.py
@@ -0,0 +1,22 @@
+import sys
+num_input = int(sys.stdin.readline())
+num_list = []
+for i in range(num_input):
+    num_list.append(list(map(int,sys.stdin.readline().split())))
+
+
+
+
+for i in range(1,len(num_list)):
+    dis = len(num_list[i])
+    for j in range(dis):
+        if j == 0:
+            num_list[i][j] = num_list[i][j] + num_list[i-1][0]
+        elif j == dis-1:
+            num_list[i][j] = num_list[i][j] + num_list[i-1][-1]
+        else:
+            num_list[i][j] = max(num_list[i][j]+num_list[i-1][j-1],num_list[i][j]+num_list[i-1][j])
+print(max(num_list[-1])) 
+
+        
+        
