d18e3bb9dd0e8ea97b7d20f95b4a32114c1befeb
rkdrud34 <rkdrud34@nate.com>
2020-08-28 19:04:47 +0900

    매크로

---
+++ macro.py
@@ -0,0 +1,94 @@
+from selenium import webdriver
+import time
+from selenium.webdriver.common.alert import Alert
+# 1. 다른 컴퓨터에서 매크로 돌릴 시 크롬 드라이버 다운 받은 뒤 경로 수정, 다수 아이디 사용시 아이디,비번 변경
+# 2. 예비 수강신청에서 실제 수강 신청으로 홈페이지 주소 바꾸기
+# 
+driver = webdriver.Chrome('/Users/kang/Downloads/chromedriver')
+driver.implicitly_wait(0.05)
+
+# driver.get("http://builder.hufs.ac.kr/user/hufs/basket_intro/basket.html")
+# sugang_button = driver.find_element_by_xpath("//*[@id='content']/div[2]/div[1]/a/div")
+# sugang_button.click()
+
+# # 수강 신청 홈페이지
+# window_after = driver.window_handles[1]
+# driver.switch_to_window(window_after)
+driver.get('http://vsugang2.hufs.ac.kr:8080/sugang/jsp/basket/s_login_go.jsp')
+while True:
+    login_input_id = driver.find_element_by_name('user_id')
+    login_input_password = driver.find_element_by_name('password')
+    login_input_id.send_keys('201602809')
+    login_input_password.send_keys('970609')
+    login_but = driver.find_element_by_xpath("/html/body/div[1]/form/div/div/div[2]/div[1]/p[3]/a[1]")
+    login_but.click()
+    #  로그인
+    driver.switch_to_frame("fra1")
+    whole = driver.find_element_by_css_selector('html > body > div:nth-child(5)')
+    whole.click()
+    
+    # print(table)
+    count=0
+    while count != 15:
+        # 장바구니 클릭
+        driver.switch_to.default_content()
+        driver.switch_to_frame("fra2")
+        driver.switch_to_frame("fra2_1")
+        button = driver.find_element_by_css_selector('body > center > div > form > table > thead > tr > th:nth-child(5) > button')
+        button.click()
+        time.sleep(0.01)
+
+    
+        driver.switch_to.default_content()
+        driver.switch_to_frame("fra2")
+        driver.switch_to_frame('fra2_2')
+        table = driver.find_elements_by_css_selector("body > center > div > form > table > tbody > tr > td:nth-child(2) > a >img" )
+        for i in table:
+            # button = i.find_element_by_css_selector("td:nth-child(2) > a > img")
+            button = i 
+            button.click()  
+            da = Alert(driver)
+            da.accept()
+            time.sleep(0.3)
+            try:
+                db  = Alert(driver)
+            except:
+                time.sleep(0.1)
+                db = Alert(driver)
+            
+            db.dismiss()
+        count+=1
+    # 로그아웃
+    driver.switch_to.default_content()
+    driver.switch_to_frame("fra1")
+    whole = driver.find_element_by_css_selector('html > body > div:nth-child(3)')
+    whole.click()
+    alert = Alert(driver)
+    time.sleep(0.02)
+    alert.accept()
+    time.sleep(0.02)
+    
+    
+
+
+    
+driver.quit()
+time.sleep(3)
+
+
+
+
+
+
+
+
+# from selenium import webdriver
+# driver = webdriver.Chrome('/Users/kang/Downloads/chromedriver')
+# driver.implicitly_wait(1)
+# driver.get("file:///Users/kang/Downloads/test_/card.html")
+# driver.implicitly_wait(3)
+# element1 = driver.find_element_by_class_name('cardContainer') 
+# element_png = element1.screenshot_as_png 
+# with open("/Users/kang/Downloads/test_/test1.png", "wb") as file: 
+#     file.write(element_png)
+# driver.quit()
--- 백준/9370_미확인도착지.py
+++ 백준/9370_미확인도착지.py
@@ -1,9 +1,7 @@
 import sys
 import heapq
-sys.setrecursionlimit(100000000)
-test_case = int(sys.stdin.readline())
-final_list = []
-for _ in range(test_case):
+final = []
+for _ in range(int(sys.stdin.readline())):
     dot_num, line_num, cand = list(map(int,sys.stdin.readline().split()))
     start_node, need_node_start, need_node_end = list(map(int,sys.stdin.readline().split()))
     all_dict = dict()
@@ -12,41 +10,168 @@ for _ in range(test_case):
         all_dict[i+1] = dict()
     for _ in range(line_num):
         temp_start,temp_end,temp_weight = list(map(int,sys.stdin.readline().split()))
-
         all_dict[temp_start][temp_end] =temp_weight
         all_dict[temp_end][temp_start] = temp_weight
     for _ in range(cand):
-        cand_list.append(int(sys.stdin.readline()))
-    
+        cand_list.append(int(sys.stdin.readline()))    
+    cand_list.sort() 
+    # print(all_dict,cand_list)
     def dijk(graph,start,cand_list):
         distances = {dot:[float('inf'),0] for dot in graph}
-        distances[start][0],distances[start][1] = 0, start
+        distances[start][0] = 0
+        
         queue = []
         heapq.heappush(queue,[distances[start][0],start])
+        print(graph)
+        print(distances)
         while queue:
-            current_distance, current_node = heapq.heappop(queue)
-            if distances[current_node][0] < current_distance:
-                continue
-            for adjacent, weight in graph[current_node].items():
-                temp_distance = current_distance + weight
-                if temp_distance < distances[adjacent][0]:
-                    distances[adjacent][0] = temp_distance
-                    distances[adjacent][1] =current_node
-                    heapq.heappush(queue, [temp_distance, adjacent])
-        result_list = []
-        for i in cand_list:
-            check_list = [i]
-            check_start = start
-            check_end = i 
-            while check_end != check_start:
-                check_end = distances[check_end][1]
-                check_list.append(check_end)
-            if need_node_start in check_list and need_node_end in check_list:
-                result_list.append(str(i))
-        final_list.append(result_list)
-    dijk(all_dict,start_node,cand_list)
-for nom in final_list:
-    print(" ".join(sorted(nom)))
+            current_distances,current_start =  heapq.heappop(queue)
+            
+            for i in graph[current_start]:
+                if distances[i][0] < graph[current_start][i]+current_distances:
+                    continue
+                if distances[i][0] > graph[current_start][i]+current_distances:
+                    distances[i][0] = graph[current_start][i]+current_distances
+                    distances[i][1] = current_start
+                    heapq.heappush(queue, [distances[i][0],i])
+            print(distances,queue)
+        return distances
+                    
+
+    result = dijk(all_dict, start_node, cand_list)
+    result_list = []
+    # for i in cand_list:
+    #     if distances[i]
+    check_list= [False]*len(cand_list)
+    for i in range(len(cand_list)):
+        if need_node_start  in result[cand_list[i]][2:] and need_node_end in result[cand_list[i]][2:]:
+            check_list[i] = True
+            result_list.append(cand_list[i])
+        
+    final.append(result_list)
+    
+
+
+for i in final:
+    print(*i)
+
+
+
+
+
+
+
+# import sys
+# import heapq
+# final = []
+# for _ in range(int(sys.stdin.readline())):
+#     dot_num, line_num, cand = list(map(int,sys.stdin.readline().split()))
+#     start_node, need_node_start, need_node_end = list(map(int,sys.stdin.readline().split()))
+#     all_dict = dict()
+#     cand_list = list()
+#     for i in range(dot_num):
+#         all_dict[i+1] = dict()
+#     for _ in range(line_num):
+#         temp_start,temp_end,temp_weight = list(map(int,sys.stdin.readline().split()))
+#         all_dict[temp_start][temp_end] =temp_weight
+#         all_dict[temp_end][temp_start] = temp_weight
+#     for _ in range(cand):
+#         cand_list.append(int(sys.stdin.readline()))    
+#     cand_list.sort() 
+#     # print(all_dict,cand_list)
+#     def dijk(graph,start,cand_list):
+#         distances = {dot:[float('inf')] for dot in graph}
+#         distances[start][0] = 0
+#         distances[start].append(start)
+#         queue = []
+#         heapq.heappush(queue,[distances[start][0],start])
+#         print(graph)
+#         print(distances)
+#         while queue:
+#             current_distances,current_start =  heapq.heappop(queue)
+#             distances[current_start].append(current_start)
+#             for i in graph[current_start]:
+#                 if distances[i][0] < graph[current_start][i]+current_distances:
+#                     continue
+#                 if distances[i][0] > graph[current_start][i]+current_distances:
+#                     distances[i][0] = graph[current_start][i]+current_distances
+#                     # distances[i].append(current_start)
+#                     distances[i].extend(distances[current_start][1:]+[current_start])
+#                     heapq.heappush(queue, [distances[i][0],i])
+#             print(distances,queue)
+#         return distances
+                    
+
+#     result = dijk(all_dict, start_node, cand_list)
+#     result_list = []
+#     # for i in cand_list:
+#     #     if distances[i]
+#     check_list= [False]*len(cand_list)
+#     for i in range(len(cand_list)):
+#         if need_node_start  in result[cand_list[i]][2:] and need_node_end in result[cand_list[i]][2:]:
+#             check_list[i] = True
+#             result_list.append(cand_list[i])
+#         # if need_node_end in result[cand_list[i]][2:] :
+#         #     check_list[i] = True
+#     # for i in range(len(check_list)):
+#     #     if check_list[i]==True:
+#     #         result_list.append(cand_list[i])
+#     final.append(result_list)
+    
+
+
+# for i in final:
+#     print(*i)
+
+
+# import sys
+# import heapq
+# sys.setrecursionlimit(100000000)
+# test_case = int(sys.stdin.readline())
+# final_list = []
+# for _ in range(test_case):
+#     dot_num, line_num, cand = list(map(int,sys.stdin.readline().split()))
+#     start_node, need_node_start, need_node_end = list(map(int,sys.stdin.readline().split()))
+#     all_dict = dict()
+#     cand_list = list()
+#     for i in range(dot_num):
+#         all_dict[i+1] = dict()
+#     for _ in range(line_num):
+#         temp_start,temp_end,temp_weight = list(map(int,sys.stdin.readline().split()))
+#         all_dict[temp_start][temp_end] =temp_weight
+#         all_dict[temp_end][temp_start] = temp_weight
+#     for _ in range(cand):
+#         cand_list.append(int(sys.stdin.readline()))
+    
+#     def dijk(graph,start,cand_list):
+#         distances = {dot:[float('inf'),0] for dot in graph}
+#         distances[start][0],distances[start][1] = 0, start
+#         queue = []
+#         heapq.heappush(queue,[distances[start][0],start])
+#         while queue:
+#             current_distance, current_node = heapq.heappop(queue)
+#             if distances[current_node][0] < current_distance:
+#                 continue
+#             for adjacent, weight in graph[current_node].items():
+#                 temp_distance = current_distance + weight
+#                 if temp_distance < distances[adjacent][0]:
+#                     distances[adjacent][0] = temp_distance
+#                     distances[adjacent][1] =current_node
+#                     heapq.heappush(queue, [temp_distance, adjacent])
+#         result_list = []
+#         for i in cand_list:
+#             check_list = [i]
+#             check_start = start
+#             check_end = i 
+#             while check_end != check_start:
+#                 check_end = distances[check_end][1]
+#                 check_list.append(check_end)
+#             if need_node_start in check_list and need_node_end in check_list:
+#                 result_list.append(str(i))
+#         final_list.append(result_list)
+#     dijk(all_dict,start_node,cand_list)
+# for nom in final_list:
+#     print(" ".join(sorted(nom)))
     
 
     
