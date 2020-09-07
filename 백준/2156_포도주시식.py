86313b9d45b55656741c2f3826fe5f095bbbb704
rkdrud34 <rkdrud34@nate.com>
2020-09-07 19:26:56 +0900

    포도주시식

--- 백준/2156_포도주시식.py
+++ 백준/2156_포도주시식.py
@@ -4,11 +4,6 @@ result_list = [0,0,0]+([0]*num)
 check_list = [0,0,0]
 for i in range(num):
     check_list.append(int(sys.stdin.readline()))
-
 for i in range(3,len(result_list)):
-    result_list[i] = max(result_list[i-2]+check_list[i], result_list[i-3]+check_list[i-1]+check_list[i])
-
-    
-
-
+    result_list[i] = max(result_list[i-1],result_list[i-2]+check_list[i], result_list[i-3]+check_list[i-1]+check_list[i])
 print(max(result_list))
