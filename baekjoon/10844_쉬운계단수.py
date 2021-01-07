06181e9aee95b05a9b06759ead82806809c7e068
rkdrud34 <rkdrud34@nate.com>
2021-01-07 18:59:01 +0900

    쉬운계단수 추가

--- baekjoon/10844_쉬운계단수.py
+++ baekjoon/10844_쉬운계단수.py
@@ -1,1 +1,17 @@
-sd
+import sys
+case = int(sys.stdin.readline())
+dp = [0]+[1 for i in range(1, 10)]
+
+for i in range(case-1):
+    temp_list = [0 for i in range(10)]
+    for j in range(len(dp)):
+        if j == 0:
+            if dp[j] != 0:
+                temp_list[j+1] += dp[j]
+        elif j == 9:
+            temp_list[j-1] += dp[j]
+        else:
+            temp_list[j+1] += dp[j]
+            temp_list[j-1] += dp[j]
+    dp = temp_list
+print(sum(dp) % 1000000000)
