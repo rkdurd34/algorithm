ed04b6b0365d9fd49f3bcb304fc0962b000261f8
rkdrud34 <rkdrud34@nate.com>
2021-01-12 13:37:06 +0900

    백준 1011번, 1182번

--- baekjoon/1011_flymetothealphacentauri.py
+++ baekjoon/1011_flymetothealphacentauri.py
@@ -2,4 +2,41 @@ import sys
 case = int(sys.stdin.readline())
 for i in range(case):
     start, end = list(map(int, sys.stdin.readline().split()))
-    print(start, end)
+    length = end - start
+    if length <= 3:
+        print(length)
+    else:
+        a = int(length ** 0.5)
+        if length == a ** 2:
+            print(2*a - 1)
+        elif a**2 < length <= a**2 + a:
+            print(2 * a)
+        else:
+            print(2*a+1)
+
+
+# 메모리 초과 풀이
+# import sys
+# case = int(sys.stdin.readline())
+# for i in range(case):
+#     start, end = list(map(int, sys.stdin.readline().split()))
+#     length = end-start - 2
+#     check = [[[1, 1]]] + [[] for _ in range(length)] + [[]]
+#     for i in range(len(check)-1):
+#         print(check)
+#         for j in range(len(check[i])):
+#             if i == len(check)-2 and check[i][j][0] < 3:
+#                 print(check[i][j][1] + 1)
+#                 break
+#             if check[i][j][0] - 1 > 0 and i + check[i][j][0]-1 < len(check):
+#                 if [check[i][j][0]-1, check[i][j][1]+1] not in check[i + check[i][j][0] - 1]:
+#                     check[i + check[i][j][0] -
+#                           1].append([check[i][j][0]-1, check[i][j][1]+1])
+#             if i + check[i][j][0] + 1 < len(check):
+#                 if [check[i][j][0]+1, check[i][j][1]+1] not in check[i + check[i][j][0] + 1]:
+#                     check[i + check[i][j][0] +
+#                           1].append([check[i][j][0]+1, check[i][j][1]+1])
+#             if i + check[i][j][0] < len(check):
+#                 if [check[i][j][0], check[i][j][1]+1] not in check[i + check[i][j][0]]:
+#                     check[i+check[i][j][0]
+#                           ].append([check[i][j][0], check[i][j][1]+1])
